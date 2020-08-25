26b812544440cdd9bd68715030d79df22708452e
Kristi McGrath <mcgrathkristi@gmail.com>
2020-08-25 11:22:41 -0400

    Updates

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,6 +1,7 @@
+#Import dependencies and setup
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import mars_scrape
+import mars_scrape #Bringing in mars_scrape.py file
 
 app = Flask(__name__)
 
@@ -8,15 +9,17 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mission_to_mars_app" 
 mongo = PyMongo(app)
 
+#Creating app route / and defining function
 @app.route("/")
 def index():
     mars_data = mongo.db.mars_data.find_one()
     return render_template("index.html", mars_data = mars_data)
 
+#Creating app route /scrape and defining function
 @app.route("/scrape")
 def scraper():
     mars_data  = mongo.db.mars_data 
-    mars_data2 = mars_scrape.scrape() # automatically equal to what scrape() returns
+    mars_data2 = mars_scrape.scrape() # automatically equal to what scrape() returns -- double check this
     mars_data.update({}, mars_data2, upsert=True)
     return redirect("/")
 
